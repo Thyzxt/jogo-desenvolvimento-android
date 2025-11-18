7123ef12d97babeedf7583e252493e02481f7813
Eduardo Naoharu <edutamogami@gmail.com>
2025-11-18 08:36:38 -0300

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    // Define a versão do plugin do Android
+    id("com.android.application") version "8.4.1" apply false
+
+    // Define a versão do Kotlin
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+
+    // Define o plugin do Compose Compiler
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+
+    // Define o plugin do Kapt
+    id("org.jetbrains.kotlin.kapt") version "2.0.0" apply false
+
+    // Define o plugin do Google Services
     id("com.google.gms.google-services") version "4.4.4" apply false
 }
